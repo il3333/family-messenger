984e3b170f42bf73b21551c2aaa9864e11d0d3e3
il3333 <il3333@mail.ru>
2026-07-02 17:33:30 +0600

    Create Color.kt

---
+++ app/app/src/main/java/com/megachat/app/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.megachat.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Peach = Color(0xFFFFF5E6)
+val PeachDark = Color(0xFFFFE0B2)
+val PeachVibrant = Color(0xFFFFCC80)
+val Cream = Color(0xFFFFF8F0)
+val Mint = Color(0xFFE8F5E9)
+val MintDark = Color(0xFFC8E6C9)
+val Lavender = Color(0xFFF3E5F5)
+val LavenderDark = Color(0xFFE1BEE7)
+val WarmBrown = Color(0xFF5D4037)
+val WarmGray = Color(0xFF8D6E63)
+val WarmGrayLight = Color(0xFFD7CCC8)
+val White = Color(0xFFFFFFFF)
+val OffWhite = Color(0xFFFEFEFE)
+val LightGray = Color(0xFFF5F5F5)
+val TextPrimary = Color(0xFF3E2723)
+val TextSecondary = Color(0xFF6D4C41)
