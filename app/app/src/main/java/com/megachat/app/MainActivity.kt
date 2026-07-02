378b30f88fcf5725e26cf5030960cabea87c2411
il3333 <il3333@mail.ru>
2026-07-02 17:32:22 +0600

    Create MainActivity.kt

---
+++ app/app/src/main/java/com/megachat/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.megachat.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.megachat.app.navigation.NavGraph
+import com.megachat.app.ui.theme.MegachatTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MegachatTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
